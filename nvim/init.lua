b0442b668f7c9969926ce06e3547dc7b9eca4c90
Ilkka Poutanen <ilkka@ilkka.dev>
2025-01-05 16:14:17 +0200

    Don't set lazyredraw globally
    This is meant to only be set during operations where updates could cause
    flicker.

--- nvim/init.lua
+++ nvim/init.lua
@@ -25,7 +25,6 @@ opt.path = vim.o.path .. '**'
 opt.number = true
 opt.relativenumber = true
 opt.cursorline = true
-opt.lazyredraw = true
 opt.showmatch = true -- Highlight matching parentheses, etc
 opt.incsearch = true
 opt.hlsearch = true
