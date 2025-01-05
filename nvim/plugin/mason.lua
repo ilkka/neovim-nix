6759556885c3fc26e583fa1afd59de75e436f689
Ilkka Poutanen <ilkka@ilkka.dev>
2025-01-05 14:04:34 +0200

    Remember to actually set up langservers

--- nvim/plugin/mason.lua
+++ nvim/plugin/mason.lua
@@ -5,3 +5,15 @@ vim.g.did_load_mason_plugin = true
 
 require('mason').setup()
 require('mason-lspconfig').setup()
+
+-- enable auto setup for LSPs
+require('mason-lspconfig').setup_handlers {
+  -- default handler that gets called for every lsp
+  function (server_name) 
+    require('lspconfig')[server_name].setup {}
+  end,
+
+  -- additional handler per LSP if wanting to do something more
+  ["rust_analyzer"] = function ()
+  end
+}
