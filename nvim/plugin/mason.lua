d839144fc1d0d10435c083a739fc50135af30163
Ilkka Poutanen <ilkka@ilkka.dev>
2025-04-02 21:52:35 +0300

    Kotlin language server config

--- nvim/plugin/mason.lua
+++ nvim/plugin/mason.lua
@@ -9,11 +9,25 @@ require('mason-lspconfig').setup()
 -- enable auto setup for LSPs
 require('mason-lspconfig').setup_handlers {
   -- default handler that gets called for every lsp
-  function (server_name) 
+  function (server_name)
     require('lspconfig')[server_name].setup {}
   end,
 
   -- additional handler per LSP if wanting to do something more
   ["rust_analyzer"] = function ()
-  end
+  end,
+
+  ["kotlin_language_server"] = function ()
+    require('lspconfig')["kotlin_language_server"].setup({
+      settings = {
+        kotlin = {
+          compiler = {
+            jvm = {
+              target = "21"
+            }
+          }
+        }
+      }
+    })
+  end,
 }
